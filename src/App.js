4d3a14e2201f016e46d85c079883af44925f6612
khajez <khajez.e@gmail.com>
2019-10-30 21:52:04 +0100

    end of module 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Player from './Player/Player';
 
-function App() {
+const App = props => {
+  const  [ playerState, setPlayersState ] = useState({
+    players: [
+      {name: "Patrick Kane", jersey: 88},
+      {name: "Marian Hossa", jersey: 81},
+      {name: "Jonathan Toews", jersey: 19},
+      {name: "Alex DeBrincat", jersey: 12}
+    ],
+    otherState: 'some other state'
+  });
+
+  const switchNameHandler = () => {
+    //console.log('button clicked.');
+    setPlayersState({
+      players: [
+        {name: "P. Kane", jersey: 88},
+        {name: "M. Hossa", jersey: 81},
+        {name: "J. Toews", jersey: 19},
+        {name: "A. DeBrincat", jersey: 12}
+      ]
+    });
+  };
+
+  const nameChangedHandler = (event) => {
+    setPlayersState({
+      players: [
+        {name: event.target.value, jersey: 88},
+        {name: "M. Hossa", jersey: 81},
+        {name: "J. Toews", jersey: 19},
+        {name: "A. DeBrincat", jersey: 12}
+      ]
+    });   
+  };
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="App">
+        <h1> Go Blackhawks!</h1>
+        <p>One Goal!</p>
+        <button style={style} onClick={switchNameHandler}>Switch Name</button>
+        <Player
+          name={playerState.players[0].name} 
+          jersey={playerState.players[0].jersey}
+          changed={nameChangedHandler} > Right Winger</Player>
+
+        <Player name={playerState.players[1].name} jersey={playerState.players[1].jersey}/>
+        <Player name={playerState.players[2].name} jersey={playerState.players[2].jersey}/>
+        <Player name={playerState.players[3].name} jersey={playerState.players[3].jersey}/>
+      </div>
+    );
+};
 
 export default App;
+
+
+
